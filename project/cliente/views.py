d0ebe1f58b36c7b3a8c7ffef9a43644cdd5efa6e
Juli tosso <julitosso88@gmail.com>
2023-09-26 14:40:00 -0300

    update index

--- project/cliente/views.py
+++ project/cliente/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from . import models
 
 def index(request):
-    contexto = {"nombre": "Cliente"}
-    return render(request, "cliente/index.html", contexto)
+    clientes = models.Cliente.objects.all()
+
+    return render(request, "cliente/index.html", {"clientes":clientes})
